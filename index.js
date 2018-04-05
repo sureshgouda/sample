80bc83d035f35f428d2bb3cf1d7282ac654fb1bf
suresh <sureshgouda.h@robosoftin.com>
2018-04-05 12:49:32 +0530

    updated

--- index.js
+++ index.js
@@ -648,7 +648,7 @@ server.post("/marcedes", function (req, res) {
           }, {
             "payload": {
               "sequenceId": "",
-              "content": `${matahisReportType} Report is generated based on following information. Now you can download.`,
+              "content": `${brand} Report is generated based on following information. Now you can download.`,
               "report": [
                 {
                   "reportLink":{
@@ -672,7 +672,7 @@ server.post("/marcedes", function (req, res) {
           }, {
             "payload": {
               "sequenceId": "",
-              "content": `${vivekReportType} Report is generated based on following information. Now you can download.`,
+              "content": `${brand} Report is generated based on following information. Now you can download.`,
               "report": [
                 {
                   "reportLink":{
